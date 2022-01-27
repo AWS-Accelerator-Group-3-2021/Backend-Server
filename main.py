5d7858e932850b651c37feef76bd33e8e21ce886
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2022-01-28 00:24:55 +0800

    added more items to combustible list

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def upload_image():
     #open combustiblelist.txt
     combustible_count = 0
     with open("combustiblelist.txt") as file:
-        lines = [line.strip() for line in file]  
+        lines = [line.strip() for line in file] 
 
     for i in lines:
         if i in names:
