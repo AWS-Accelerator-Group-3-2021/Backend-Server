294b5cfaab565802f22ca8f7ae04c3fb330431e9
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2022-07-23 17:26:35 +0800

    changed hardcoded bucket name to env variable

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def clearFiles():
     # s3 = session.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
     #                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     s3Resource = boto3.resource('s3')
-    bucket = s3Resource.Bucket('combustifierbucket')
+    bucket = s3Resource.Bucket(BUCKET_NAME)
     bucket.objects.all().delete()
     return "Cleared all files from AWS S3 Bucket successfully!"
 
